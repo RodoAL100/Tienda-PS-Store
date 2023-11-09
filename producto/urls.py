0e19043a58ced4af224c07a31eecb8451f933960
Rodo <la063303203@unab.edu.sv>
2023-11-09 01:45:38 -0300

    subida final

---
+++ producto/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from producto import views
+
+urlpatterns = [
+    path('', views.inicio, name="inicio"),
+    path('productos/', views.listado_productos, name="productos"),
+    path('productos/crear', views.crear, name="crear"),
+    path('productos/editar/<int:id>', views.editar_producto, name="editar_producto"),
+    path('productos/eliminar/<int:id>', views.eliminar_producto, name="eliminar_producto"),
+    path('productos/acerca_de/', views.acerca_de, name='acerca_de'),
+]
